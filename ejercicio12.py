60ca1e8a6a69053f85d9e92eb5fc1fb639f2948b
shar0makai <131774311+shar0makai@users.noreply.github.com>
2023-06-28 12:52:15 -0300

    Delete ejercicio12.py

--- ejercicio12.py
+++
@@ -1,7 +0,0 @@
-fecha=input('Ingrese la fecha ACTUAL en formato DD/MM/AAAA: ')
-nacimiento=input('Ingrese la fecha de NACIMIENTO en formato DD/MM/AAAAA: ')
-
-lista1=fecha.split("/")
-lista2=nacimiento.split("/")
-
-print(f'La edad aprox. es de: {int(lista1[2])-int(lista2[2])}')
